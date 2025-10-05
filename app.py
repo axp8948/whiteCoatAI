856156c288f2d11a218e1e3677cfab9ff7eb4daf
Anmol Pandey <axp8948@mavs.uta.edu>
2025-10-05 16:19:35 -0500

    Fixed Gemini's Model Error

--- app.py
+++ app.py
@@ -14,7 +14,11 @@ from db import get_report, add_chat, get_report_stats
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-1.5-flash")
+
+# for m in genai.list_models():
+#     print(m.name, m.supported_generation_methods)
+
+model = genai.GenerativeModel("models/gemini-flash-latest")
 
 # Set page configuration
 st.set_page_config(
